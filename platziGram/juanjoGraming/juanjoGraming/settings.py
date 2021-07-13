db5f8015d07621584d0f0abe0f6cbb0b709d4969
Juan Jose Estrada <juan5801331@gmail.com>
2021-07-12 20:17:13 -0700

    cada ves estoy mas cerca de terminar
    B

--- platziGram/juanjoGraming/juanjoGraming/settings.py
+++ platziGram/juanjoGraming/juanjoGraming/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'juanjoGraming.middleware.ProfileCompletionMiddleware',
 ]
 
 ROOT_URLCONF = 'juanjoGraming.urls'
@@ -59,7 +60,9 @@ ROOT_URLCONF = 'juanjoGraming.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +126,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    (BASE_DIR / 'static'),
+)
+
+STATICFILES_FINDERS=[
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -131,3 +143,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT=  BASE_DIR / 'media'
 MEDIA_URL='/media/'
+
+LOGIN_URL='/users/login/'
--- platziGram/juanjoGraming/posts/views.py
+++ platziGram/juanjoGraming/posts/views.py
@@ -1,46 +1,40 @@
 """ Posts views"""
 #Django imports
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 #utilities
 from datetime import datetime
+#models
+from posts.models import Post
+#forms
+from posts.forms import PostForm
 #Creo Diccionario de Posts
 
-posts=[{
-    'title': 'Fiesta Loca',
-    'user':{ 
-        'name':'jjestrada',
-        'picture':'https://www.pexels.com/photo/black-and-white-cat-in-front-of-black-and-white-front-load-washing-machine-4975907/'
-        },
-        'timestamp': datetime.now(),
-        'photo':'https://www.pexels.com/photo/animal-big-fur-zoo-4468189/' 
 
-    },
-    {
-    'title': 'Viaje a NY',
-    'user':{ 
-        'name':'Cvanjercito',
-        'picture':'https://www.pexels.com/photo/topless-man-with-black-eyes-7519434/'
 
-        },
-    'timestamp': datetime.now(),
-    'picture':'https://images.unsplash.com/photo-1624215824600-ed3118b76873?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80'
-    
-
-    },
-    {
-    'title': 'Primer dia de clases',
-    'user':{ 
-        'name':'jpcuervo',
-        'picture':'https://images.pexels.com/photos/4550259/pexels-photo-4550259.jpeg?cs=srgb&dl=pexels-salus-natura-suprema-lex-esto-4550259.jpg&fm=jpg'
-
-        },
-    'timestamp': datetime.now(),
-    'picture':'https://images.pexels.com/photos/4436390/pexels-photo-4436390.jpeg?cs=srgb&dl=pexels-el%C3%ADas-manuel-4436390.jpg&fm=jpg'
-    
-    
-
-    },
-    ]
- 
+@login_required
 def list_posts(request):
-    return render(request,'feed.html',{'posts':posts})
+    """List existing posts."""
+    posts=Post.objects.all().order_by('-created')
+    return render(request, 'posts/feed.html', {'posts': posts})
+
+
+
+@login_required
+def create_post(request):
+    if request.method=='POST':
+        form=PostForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('feed')
+
+    else:
+        form=PostForm()
+    return render(
+        request=request,
+        template_name='posts/new.html',
+        context={
+        'form': form,
+        'user':request.user,
+        'profile':request.user.profile,}
+    )    
